3efc4fe7a66cbde5ea1a09d917510ed2810d61bb
Marvinsky <marvin.abisrror@gmail.com>
2020-11-12 19:00:25 -0500

    add intent

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 2;
+    int whipped_cream_price = 1;
+    int chocolate_price = 2;
 
     /**
     THis app displays an order form to order coffee
@@ -24,8 +32,34 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         int price = 5;
-        String priceMessage = createOrderSummary(price);
-        displayMessage(priceMessage);
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocalateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        EditText nameEditText = (EditText) findViewById(R.id.name_edit_text_view);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        boolean hasChocalate = chocalateCheckBox.isChecked();
+        String name = nameEditText.getText().toString();
+        Log.v(this.getClass().getName(), "Has whipped cream: " + hasWhippedCream);
+        Log.v(this.getClass().getName(), "Has chocolate: "+ hasChocalate);
+        Log.v(this.getClass().getName(), "Name: " + name);
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocalate);
+
+        /*
+        Handle Maps View
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:-12.1353916,-77.0241859"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        */
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        //intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Order for "+ name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 
@@ -43,31 +77,32 @@ public class MainActivity extends AppCompatActivity {
      * @param price of the order
      * @return Summary
      */
-    private String createOrderSummary(int price) {
-        String priceMessage = "Name: Kaptain Kunal";
+    private String createOrderSummary(String name, int price, boolean hasWhippedCream, boolean hasChocolate) {
+        String priceMessage = "Name: "+ name;
         priceMessage += "\nQuantity: " + quantity;
-        priceMessage += "\nTotal: $" + price*quantity;
+        priceMessage += "\nhas whipped cream: " + (hasWhippedCream ? "Yes" : "No");
+        priceMessage += "\nhas chocolate: " + (hasChocolate ? "Yes" : "No");
+        priceMessage += "\nTotal: $" + (price*quantity + (hasWhippedCream ? whipped_cream_price : 0) + (hasChocolate ? chocolate_price : 0));
         priceMessage += "\nThank you!";
         return priceMessage;
     }
 
-
     public void increment(View view) {
+
+        if (quantity == 100) {
+            Toast.makeText(this, "You cannot have more than 100 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
-        quantity = quantity - 1;
-        if (quantity < 0) {
-            quantity = 0;
+        if (quantity == 1) {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
         }
-
+        quantity = quantity - 1;
         displayQuantity(quantity);
-    }
-
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView)findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
     }
 }
